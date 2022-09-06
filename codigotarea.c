951b30c5e57bf008a54ed9ead3f39707f6dcbdc4
AlfredoGuadalupe <54385258+AlfredoGuadalupe@users.noreply.github.com>
2022-09-06 14:47:01 -0500

    Update codigotarea.c

--- codigotarea.c
+++ codigotarea.c
@@ -12,7 +12,7 @@ int main(){
 				printf("Hola desde la opcion 1\n\n");
 				break;
 			case 2:
-				printf("Hola desde la opción 2\n\n")
+				printf("Hola desde la opción 2\n\n");
 				break;
 			case 3:
 				printf("Hola desde la opción 3\n\n");
